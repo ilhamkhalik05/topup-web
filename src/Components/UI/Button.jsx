c82a71bd1f15e94ac7b53e16db8b9c9875460ff4
Ilham Khalik <believerrr05@gmail.com>
2024-06-05 07:25:12 +0700

    Delete components/ui folder

--- src/Components/UI/Button.jsx
+++
@@ -1,3 +0,0 @@
-export default function Button({ classname = '', children, onClick = null }) {
-   return <button className={classname} onClick={onClick}>{children}</button>
-}
--- src/Components/UI/Image.jsx
+++
@@ -1,5 +0,0 @@
-export default function Image({ className, src, alt, width, height }) {
-   return (
-      <img className={className} src={src} alt={alt} width={width} height={height} />
-   )
-}
