4e03bc8b1d0fc41255e7833d3dfc44c8e499ae48
Ilham Khalik <believerrr05@gmail.com>
2024-06-05 07:22:39 +0700

    Make a responsive navbar layout

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,5 @@
-import { FaToggleOff, FaToggleOn } from "react-icons/fa";
-import { FaSearch } from "react-icons/fa";
-import { FaArrowRightFromBracket } from "react-icons/fa6";
-import { FaRegUser } from "react-icons/fa6";
+import { FaToggleOff, FaToggleOn, FaSearch } from "react-icons/fa";
+import { FaArrowRightFromBracket, FaRegUser, FaBars } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import logo from '../Asset/logo.webp'
@@ -9,25 +7,31 @@ import logo from '../Asset/logo.webp'
 export default function Navbar({ theme, toggleTheme }) {
    return (
       <nav className={`navbar ${theme === 'light' ? 'bg-zinc-200 text-zinc-900' : 'bg-zinc-900 text-zinc-200'} z-10 sticky top-0 text-white shadow-md shadow-yellow-200`}>
-         <div className="container flex justify-between items-center mx-auto p-3">
+         <div className="container flex justify-between items-center mx-auto p-5 lg:p-3">
+            {/* NAV TOGGLER */}
+            <div className="nav-toggler block lg:hidden">
+               <FaBars />
+            </div>
+
+            {/* NAV BRAND AND NAVIGATION */}
             <div className="nav-start flex items-center gap-7">
                <img src={logo} alt="Logo" width={150} />
-               <div className="nav-menu flex items-center gap-5 text-zinc-300 border-s-2 border-yellow-300 pl-5">
+               <div className={`nav-menu hidden ${theme === 'light' ? 'text-zinc-800' : 'text-zinc-300'} lg:flex items-center gap-5 border-s-2 border-yellow-300 pl-5`}>
                   <a className="hover:text-yellow-200" href="">Home</a>
                   <a className="hover:text-yellow-200" href="">Daftar Layanan</a>
                   <a className="hover:text-yellow-200" href="">Cek Transaksi</a>
                </div>
             </div>
+
+            {/* NAV SEARCH AND NAV AUTH WITH THEME TOGGLER */}
             <div className="nav-end flex items-center gap-4">
                <NavSearch />
-
                {theme === 'light'
                   ?
-                  <FaToggleOn className="cursor-pointer text-2xl font-bold" onClick={toggleTheme} />
+                  <FaToggleOn className="hidden lg:block cursor-pointer text-2xl font-bold" onClick={toggleTheme} />
                   :
-                  <FaToggleOff className="cursor-pointer text-2xl font-bold" onClick={toggleTheme} />
+                  <FaToggleOff className="hidden lg:block cursor-pointer text-2xl font-bold" onClick={toggleTheme} />
                }
-
                <NavAuth />
             </div>
          </div>
@@ -66,12 +70,16 @@ const NavSearch = () => {
 
 const NavAuth = () => {
    return (
-      <div className="user flex items-center gap-2">
-         <Link className="px-5 py-2 bg-primary text-black rounded-lg glass flex items-center gap-2 hover:bg-yellow-500 ease-in duration-150" href="">
+      <div className="user hidden lg:flex items-center gap-2">
+         <Link
+            className="px-5 py-2 bg-primary text-black rounded-lg glass flex items-center gap-2 hover:bg-yellow-500 ease-in duration-150"
+            href="/login">
             <FaArrowRightFromBracket />
             Masuk
          </Link>
-         <Link className="px-5 py-2 bg-primary text-black rounded-lg glass flex items-center gap-2 hover:bg-yellow-500 ease-in duration-150" href="">
+         <Link
+            className="px-5 py-2 bg-primary text-black rounded-lg glass flex items-center gap-2 hover:bg-yellow-500 ease-in duration-150"
+            href="/signup">
             <FaRegUser />
             Daftar
          </Link>
