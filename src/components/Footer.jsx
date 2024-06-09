6b9a4666cbf6c46512729ba629f4fba5bf1fcab1
Ilham Khalik <believerrr05@gmail.com>
2024-06-09 09:09:32 +0700

    Make footer light mode

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,26 +2,26 @@ import logo from '../assets/logo.webp'
 
 export default function Footer() {
    return (
-      <footer className="footer p-10 bg-zinc-950 mt-10">
+      <footer className="footer p-10 mt-10 bg-zinc-200 dark:bg-zinc-950">
          <aside className="flex flex-col gap-3">
             <img src={logo} alt="" width={180} height={180} />
-            <p className="text-zinc-300">WEBTOPUPKU<br />Website Top-Up Termurah, Tercepat & Terpercaya di Indonesia</p>
+            <p className="text-black dark:text-zinc-300">WEBTOPUPKU<br />Website Top-Up Termurah, Tercepat & Terpercaya di Indonesia</p>
          </aside>
-         <div className="grid grid-cols-2 lg:grid-cols-3 gap-5 w-full">
-            <nav className="flex flex-col gap-2 text-zinc-300">
+         <div className="grid grid-cols-2 lg:grid-cols-3 gap-5 w-full text-zinc-950 dark:text-zinc-300">
+            <nav className="flex flex-col gap-2">
                <h6 className="footer-title">Social Media</h6>
                <a className="link link-hover">Instagram</a>
                <a className="link link-hover">Facebook</a>
                <a className="link link-hover">Tiktok</a>
             </nav>
-            <nav className="flex flex-col gap-2 text-zinc-300">
+            <nav className="flex flex-col gap-2">
                <h6 className="footer-title">Peta Situs</h6>
                <a className="link link-hover">Tentang</a>
                <a className="link link-hover">FAQ</a>
                <a className="link link-hover">Syarat & Ketentuan</a>
                <a className="link link-hover">Layanan</a>
             </nav>
-            <nav className="flex flex-col gap-2 text-zinc-300">
+            <nav className="flex flex-col gap-2">
                <h6 className="footer-title">ketentuan</h6>
                <a className="link link-hover">Terms of use</a>
                <a className="link link-hover">Privacy policy</a>
